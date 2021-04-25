3d67434a88ae23334ef3003c216c8a67b6c32436
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2021-04-25 12:52:20 +0200

    firestore implemented in appdelegate

--- music-lists/music_listsApp.swift
+++ music-lists/music_listsApp.swift
@@ -6,10 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+    func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct music_listsApp: App {
-
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
     }
