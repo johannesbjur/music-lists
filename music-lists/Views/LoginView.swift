b09d7cad6e612e488f75285b8f1f9735f6e9d62c
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2021-05-10 11:27:51 +0200

    login ui finished

--- music-lists/Views/LoginView.swift
+++ music-lists/Views/LoginView.swift
@@ -13,24 +13,60 @@ struct LoginView: View {
             Color("black")
                 .ignoresSafeArea()
             VStack {
+                Wave()
+                    .fill(Color("green"))
+                    .frame(height: 180)
+                
+                Spacer()
+                    .frame(height: 180)
+                
                 Text("Login")
                     .font(.system(size: 32, weight: .bold, design: .default))
                     .foregroundColor(.white)
                     .padding()
                 
+                Spacer()
+                    .frame(height: 30)
+                
                 NavigationLink(
                     destination: AuthWebView()
                 ) {
-                    Text("Sign in with Spotify")
-                        .frame(width: 280, height: 50)
-                        .background(Color("green"))
-                        .foregroundColor(.white)
-                        .cornerRadius(30)
+                    HStack {
+                        Image("spotify_icon_white")
+                            .resizable()
+                            .frame(width: 25, height: 25, alignment: .center)
+                        Text("Sign in with Spotify")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    .frame(width: 260, height: 55)
+                    .background(Color("green"))
+                    .cornerRadius(30)
                 }
+                Spacer()
             }
+            .navigationBarHidden(true)
+            .edgesIgnoringSafeArea(.all)
         }
     }
 }
+
+struct Wave: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX*2, y: rect.maxY))
+        path.addCurve(to: CGPoint(x: rect.minX, y: rect.maxY*1.5),
+                      control1: CGPoint(x: rect.maxX * 0.3, y: rect.midY),
+                      control2: CGPoint(x: rect.maxX * 0.7, y: rect.maxY * 1.7))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
